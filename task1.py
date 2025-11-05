1cb51175d67ab938d448f964bdf05c18144cf77a
niklas-04 <niklas.sundin.1374@student.uu.se>
2025-11-05 12:14:48 +0100

    task 1 klar

--- task1.py
+++ task1.py
@@ -4,23 +4,23 @@ from scipy import signal
 
 #import sounddevice as sd
 
-t0 = 0.01
+t1 = 0.01
 f2 = 1000
 deltaTime = 0.01 * 10**-3
-timeVector = np.arange(0, 0.01, deltaTime)
+timeVector = np.arange(0, 5, deltaTime)
 
-#Sine wave 1
+#Sine wave 1 (x1)
 amp1 = 1
 phase1 = 0
-omega1 = 2 * np.pi / t0
+omega1 = 2 * np.pi / t1
 
-sine1 = amp1 * np.sin((omega1 + phase1))
+# x1 = amp1 * np.sin((omega1 + phase1))
 
-#Sine wave 2
+#Sine wave 2 (x2)
 amp2 = 1
 phase2 = 0
 omega2 = 2 * np.pi * f2 
-sine2 = amp2 * np.sin((omega2 + phase2))
+# x2 = amp2 * np.sin((omega2 + phase2))
 
 y1 = np.zeros_like(timeVector)
 y2 = np.zeros_like(timeVector)
@@ -30,6 +30,15 @@ for i in range(len(timeVector)):
     y2[i] = amp2 * np.sin((omega2 * timeVector[i] + phase2))
 
 fig, ax = plt.subplots()
-ax.plot(timeVector, y1, label="Sine 1")
-ax.plot(timeVector, y2, label="Sine 2")
+ax.plot(timeVector, y1, label="x1(t)")
+ax.plot(timeVector, y2, label="x2(t)")
+
+ax.set_xlabel("time")
+ax.set_ylabel("output signal")
+
+ax.legend()
+
+ax.set_xlim(0, 10 * 10**-3)
+ax.set_ylim(-1, 1)
+
 plt.show()
